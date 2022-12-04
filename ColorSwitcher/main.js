3668a312afefe53efba2ee11701d7ba597e54ed3
Mayank Joshi <104020635+Mayank0000000@users.noreply.github.com>
2022-12-04 15:09:36 +0530

    Add files via upload

---
+++ ColorSwitcher/main.js
@@ -0,0 +1,12 @@
+document.getElementById('gray').onclick = function() {
+    document.getElementsByTagName('body')[0].style.backgroundColor = 'gray';
+}
+document.getElementById('white').onclick = function() {
+    document.getElementsByTagName('body')[0].style.backgroundColor = 'white';
+}
+document.getElementById('blue').onclick = function() {
+    document.getElementsByTagName('body')[0].style.backgroundColor = 'blue';
+}
+document.getElementById('yellow').onclick = function() {
+    document.getElementsByTagName('body')[0].style.backgroundColor = 'yellow';
+}
